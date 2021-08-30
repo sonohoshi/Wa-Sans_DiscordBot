ccd3204218843c72879bf732d6e83d4ec5d06625
sonohoshi <yd111544@gmail.com>
2021-08-30 23:54:22 +0900

    Make check message system

--- SansIsAlive/BotInitializer.cs
+++ SansIsAlive/BotInitializer.cs
@@ -6,6 +6,14 @@ using Discord.WebSocket;
 
 namespace SansIsAlive
 {
+    public enum CommandCondition
+    {
+        None = -1,
+        BasicCommand = 0,
+        Wa,
+        Sans
+    }
+    
     public class BotInitializer
     {
         private static DiscordSocketClient _client;
@@ -14,7 +22,7 @@ namespace SansIsAlive
         public static async Task BotMain(string token)
         {
             _client ??= new DiscordSocketClient(new DiscordSocketConfig
-            {    
+            {
                 LogLevel = LogSeverity.Verbose                       
             });
             _commands = new CommandService(new CommandServiceConfig 
@@ -38,11 +46,15 @@ namespace SansIsAlive
             if (!(arg is SocketUserMessage message)) return;
 
             int pos = 0;
-            
-            if (!(message.HasCharPrefix('!', ref pos) || CheckMessageIsCommand(message)))
+
+            var commandType = CheckMessageCondition(message);
+
+            if (CheckMessageIsCommand(message) || commandType == CommandCondition.None)
                 return;
 
-            var context = new SocketCommandContext(_client, message);             
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(context, pos, null);
 
             await context.Channel.SendMessageAsync($"Received command : {message.Content}"); 
         }
@@ -57,6 +69,15 @@ namespace SansIsAlive
         {
             Console.WriteLine($"[{DateTime.Now}] {msg.ToString()}"); 
             return Task.CompletedTask;
+        }
+
+        public static CommandCondition CheckMessageCondition(SocketUserMessage msg)
+        {
+            int pos = 0;
+            if (msg.Content.Contains("와")) return CommandCondition.Wa;
+            if (msg.Content.Contains("샌즈")) return CommandCondition.Sans;
+            if (msg.HasCharPrefix('$', ref pos)) return CommandCondition.BasicCommand;
+            return CommandCondition.None;
         }
     }
 }
